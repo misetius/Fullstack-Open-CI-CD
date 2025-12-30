4add6c6444fe144e957f131097b512af090a3834
Miska Malkamaki <mitamalk.jyu.fi>
2025-12-30 18:31:41 +0200

    fixing e2e

--- tests/E2E-tests.spec.js
+++ tests/E2E-tests.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test')
 
 
 test('front page can be opened', async ({page}) => {
-  await page.goto('http://localhost:4173')
+  await page.goto('/')
   const locator1 = page.getByText('Phonebook')
   const locator2 = page.getByText('Numbers')
   await expect(locator1).toBeVisible()
