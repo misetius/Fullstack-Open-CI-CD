cc47beb0c4a0f710ed4af99dc684a0482bb58d76
Miska Malkamaki <mitamalk.jyu.fi>
2025-12-31 15:12:13 +0200

    test

--- tests/E2E-tests.spec.js
+++ tests/E2E-tests.spec.js
@@ -5,6 +5,7 @@ test('front page can be opened', async ({page}) => {
   await page.goto('/')
   const locator1 = page.getByText('Phonebook')
   const locator2 = page.getByText('Numbers')
+  //test
   await expect(locator1).toBeVisible()
   await expect(locator2).toBeVisible()
 }) 
